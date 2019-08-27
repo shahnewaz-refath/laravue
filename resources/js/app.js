3026825d5338d1f00c473029d7db1a4103600e7a
refath <refathctg@gmail.com>
2019-08-27 11:40:11 +0600

    Updated work

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,6 +7,56 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+import { Form, HasError, AlertError } from 'vform'
+import moment from 'moment';
+
+window.Form = Form;
+Vue.component(HasError.name, HasError)
+Vue.component(AlertError.name, AlertError)
+
+import VueRouter from 'vue-router'
+import VueProgressBar from 'vue-progressbar'
+import Swal from 'sweetalert2'
+
+window.Swal = Swal;
+const Toast = Swal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 3000
+})
+window.Toast = Toast;
+
+Vue.use(VueProgressBar,{
+  color: 'rgb(143,255,199)',
+  failedColor: 'red',
+  height: '3px',
+})
+
+window.Fire = new Vue();
+
+Vue.use(VueRouter)
+
+let routes = [
+    { path: '/dashboard', component: require('./components/Dashboard.vue').default },
+    { path: '/developer', component: require('./components/Developer.vue').default },
+    { path: '/users', component: require('./components/Users.vue').default },
+    { path: '/profile', component: require('./components/Profile.vue').default }
+  ]
+
+const router = new VueRouter({
+    mode: 'history',
+    routes // short for `routes: routes`
+  })
+
+Vue.filter('upText',function(text){
+  return text.charAt(0).toUpperCase() + text.slice(1);
+});
+
+Vue.filter('myDate',function(created){
+  return moment(created).format('MMMM Do YYYY');
+});
+
 
 /**
  * The following block of code may be used to automatically register your
@@ -19,6 +69,21 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i);
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
+Vue.component(
+  'passport-clients',
+  require('./components/passport/Clients.vue').default
+);
+
+Vue.component(
+  'passport-authorized-clients',
+  require('./components/passport/AuthorizedClients.vue').default
+);
+
+Vue.component(
+  'passport-personal-access-tokens',
+  require('./components/passport/PersonalAccessTokens.vue').default
+);
+
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
 /**
@@ -29,4 +94,5 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 
 const app = new Vue({
     el: '#app',
+    router
 });
---
+++ resources/js/components/Profile.vue
@@ -0,0 +1,167 @@
+<style>
+    .widget-user-header{
+        background-position: center center;
+        background-size: cover;
+        height: 250px;
+    }
+</style>
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 mt-3">
+            <!-- Widget: user widget style 1 -->
+                <div class="card card-widget widget-user">
+                <!-- Add the bg color to the header using any of the bg-* classes -->
+                    <div class="widget-user-header text-white" style="background: url('./img/uucover.jpg');">
+                        <h3 class="widget-user-username">Elizabeth Pierce</h3>
+                        <h5 class="widget-user-desc">Web Designer</h5>
+                    </div>
+                    <div class="widget-user-image">
+                        <img class="img-circle" src="" alt="User Avatar">
+                    </div>
+                    <div class="card-footer">
+                        <div class="row">
+                            <div class="col-sm-4 border-right">
+                                <div class="description-block">
+                                <h5 class="description-header">3,200</h5>
+                                <span class="description-text">SALES</span>
+                                </div>
+                                <!-- /.description-block -->
+                            </div>
+                            <!-- /.col -->
+                            <div class="col-sm-4 border-right">
+                                <div class="description-block">
+                                <h5 class="description-header">13,000</h5>
+                                <span class="description-text">FOLLOWERS</span>
+                                </div>
+                                <!-- /.description-block -->
+                            </div>
+                            <!-- /.col -->
+                            <div class="col-sm-4">
+                                <div class="description-block">
+                                <h5 class="description-header">35</h5>
+                                <span class="description-text">PRODUCTS</span>
+                                </div>
+                                <!-- /.description-block -->
+                            </div>
+                            <!-- /.col -->
+                        </div>
+                        <!-- /.row -->
+                    </div>
+                </div>
+                <!-- /.widget-user -->
+            </div>
+
+            <div class="col-md-12 mt-3">
+            <div class="card">
+              <div class="card-header p-2">
+                <ul class="nav nav-pills">
+                  <li class="nav-item"><a class="nav-link active" href="#activity" data-toggle="tab">Activity</a></li>
+                  <li class="nav-item"><a class="nav-link" href="#settings" data-toggle="tab">Settings</a></li>
+                </ul>
+              </div><!-- /.card-header -->
+              <div class="card-body">
+                <div class="tab-content">
+                  <div class="tab-pane active" id="activity">
+                    <h3>Display Activity</h3>
+                  </div>
+                  <!-- /.tab-pane -->
+                  
+
+                  <div class="tab-pane" id="settings">
+                    <form class="form-horizontal">
+                      <div class="form-group">
+                        <label for="inputName" class="col-sm-2 control-label">Name</label>
+
+                        <div class="col-sm-10">
+                          <input v-model="form.name" type="text" class="form-control" id="inputName" placeholder="Name">
+                        </div>
+                      </div>
+                      <div class="form-group">
+                        <label for="inputEmail" class="col-sm-2 control-label">Email</label>
+
+                        <div class="col-sm-10">
+                          <input v-model="form.email" type="email" class="form-control" id="inputEmail" placeholder="Email">
+                        </div>
+                      </div>
+                      <div class="form-group">
+                        <label for="inputExperience" class="col-sm-2 control-label">Experience</label>
+
+                        <div class="col-sm-10">
+                          <textarea class="form-control" id="inputExperience" placeholder="Experience"></textarea>
+                        </div>
+                      </div>
+                      <div class="form-group">
+                        <label for="photo" class="col-sm-2 control-label">Profile Photo</label>
+
+                        <div class="col-sm-10">
+                          <input type="file" @change="updateProfile" name="photo" id = "photo">
+                        </div>
+                      </div>
+                      <div class="form-group">
+                        <label for="password" class="col-sm-12 control-label">Password(leave empty if not changing)</label>
+
+                        <div class="col-sm-10">
+                          <input type="password" class="form-control" id="password" placeholder="password">
+                        </div>
+                      </div>
+                      <div class="form-group">
+                        <div class="col-sm-offset-2 col-sm-10">
+                          <button @click.prevent = "updateInfo" type="submit" class="btn btn-success">Update</button>
+                        </div>
+                      </div>
+                    </form>
+                  </div>
+                  <!-- /.tab-pane -->
+                </div>
+                <!-- /.tab-content -->
+              </div><!-- /.card-body -->
+            </div>
+            <!-- /.nav-tabs-custom -->
+          </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+              form: new Form({
+                    id: '',
+                    name: '',
+                    email: '',
+                    password: '',
+                    type: '',
+                    bio: '',
+                    photo: ''
+                })
+            }
+        },
+        methods:{
+          updateInfo(){
+            this.form.put('api/profile')
+            .then(() => {
+              
+            })
+            .catch(() => {
+
+            });
+          },
+          updateProfile(e){
+            let file = e.target.files[0];
+            let reader = new FileReader();
+            reader.onloadend = (file) => {
+              this.form.photo = reader.result;
+            }
+            reader.readAsDataURL(file);
+          }
+        },
+        mounted() {
+            console.log('Component mounted.')
+        },
+        created(){
+            axios.get("api/profile").then(({data}) => (this.form.fill(data)));
+        }
+    }
+</script>
